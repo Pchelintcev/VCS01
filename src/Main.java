dc68e4d354cecc471d552a05f3885a320e94aeec
Sergei Pchelintsev <s.pchelintcev@yandex.ru>
2022-07-27 18:50:35 +0300

    добавил реализацию программы

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        SalesManager week01 = new SalesManager(new int[] {15, 20, 21, 14, 16, 25, 30});
+        System.out.println("максимальные продажи за указанный период: " + week01.max());
+    }
+}
